3379ac38ca147b4dfad6c8be7c013f558b2b2c8a
usabah <uriyasabah309@gmail.com>
2023-05-05 19:45:54 -0400

    updated playlist generation flow and model

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import jsonify, Flask
 from flask_cors import CORS, cross_origin
 import json
+import scipy.stats as stats
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-from sklearn.linear_model import LogisticRegression 
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.neural_network import MLPClassifier
 import pickle
 import numpy as np
 import pandas as pd
@@ -70,12 +72,12 @@ def firestoreConnection():
         firebase_admin.initialize_app(cred)
     db = firestore.client()
     
-LR_model = None
-def load_LR_model():
-    global LR_model
-    with open('logreg_model1.pkl','rb') as f:
-        LR_model = pickle.load(f)
-    warnings.filterwarnings("ignore", message="X does not have valid feature names, but LogisticRegression was fitted with feature names")
+MLP_model = None
+def load_mlp_model():
+    global MLP_model
+    with open('MLP3.pkl','rb') as f:
+        MLP_model = pickle.load(f)
+        
 #______________________________________________
 # Playlist Generation
 #______________________________________________
@@ -90,8 +92,9 @@ def generateNewSongsList(mood, num_songs, closest_song_ids, old_songs_list): ###
         firestoreConnection()
     
     mood_index = moods.index(mood) #Numbered label of mood
+    num_songs_in_arr = num_songs #Number of songs requested to retrieve (change if model is recommender is 'struggling')
     #specify target and close moods
-    acceptable_moods = [mood_index, (mood_index + len(moods) - 1) % len(moods), (mood_index + 1)%len(moods)] 
+    acceptable_moods = [mood_index] 
     song_count = 0
     count_mood_index = 0 #TESTING TO DETERMINE RATE OF SONG ADDITION
     count_other_moods = 0
@@ -107,61 +110,64 @@ def generateNewSongsList(mood, num_songs, closest_song_ids, old_songs_list): ###
     # Extract the URIs from the track information
     track_uris = [track['uri'] for track in track_info]
     final_song_ids = []
+    number_songs_to_pass = len(track_uris[:5])
     while len(final_song_ids) < num_songs:        
         # Get recommended tracks and add them to final_song_ids if mood is classified as specified mood
-        tracks = sp.recommendations(seed_tracks=track_uris[:5], limit=50)['tracks'] ##############################LIMIT
+        tracks = sp.recommendations(seed_tracks=track_uris[:number_songs_to_pass], limit=100)['tracks'] ##############################LIMIT
         track_ids = [track["id"] for track in tracks if track["id"] not in final_song_ids and track["id"] not in old_songs_list]
         #Split ids by whether they are already labelled or not
         known_track_moods_dict = getAlreadyLabelled(track_ids)
         new_track_ids = [track_id for track_id in track_ids if track_id not in known_track_moods_dict.keys()]
         # Get song features of the new ids
         features_df = retrieveTrackFeatures(new_track_ids)
+        processed_features_df = clipAndNormalizeMLP(features_df)
         
         # Get predictions and update database
         # If there are no features or lyrics, and no songs are already labelled return an error
-        if features_df.shape[0] == 0 and len(known_track_moods_dict.keys()) == 0:
+        if processed_features_df.shape[0] == 0 and len(known_track_moods_dict.keys()) == 0:
             return {"error": "Issue with model and/or spotify server"}
         else:
             predictions = {}
             stop_loops = False
 
-            LR_pred, LR_pred_probability = getMoodLabelLR(features_df)
+            MLP_pred, MLP_pred_probability = getMoodLabelMLP(processed_features_df)
             for i, (key, row) in enumerate(features_df.iterrows()):
                 if not(stop_loops):
-                    predictions[key]=LR_pred[i]
-                    if LR_pred[i] in acceptable_moods: song_count += 1
+                    predictions[key]=MLP_pred[i]
+                    if MLP_pred[i] in acceptable_moods: song_count += 1
                     stop_loops = (song_count == num_songs)
 
-            # Add song moods to DB
-            addTrackMoodToDB(predictions)
-            
-            # Combine predictions and known labels
-            # Currently, this will prioritize adding new songs to playlist over hits in our DB
-            num_songs_remaining = num_songs - song_count
-            ###STAT DICT FOR TESTING
-            stat_dict = {**known_track_moods_dict, **predictions}
-            #Shorten list of songs if necessary, start with the known moods (will add close moods as well)
-            ids_to_add = [key for key in known_track_moods_dict.keys() 
-                          if known_track_moods_dict[key] in acceptable_moods]
-            if len(ids_to_add) > num_songs_remaining:
-                ids_to_add = ids_to_add[:num_songs_remaining]
-            song_count += len(ids_to_add)
-        
+        # Add song moods to DB
+        addTrackMoodToDB(predictions)
+
+        # Combine predictions and known labels
+        # Currently, this will prioritize adding new songs to playlist over hits in our DB
+        ###STAT DICT FOR TESTING
+        stat_dict = {**known_track_moods_dict, **predictions}
+        #Shorten list of songs if necessary, start with the known moods (will add close moods as well)
+        ids_to_add = [key for key in known_track_moods_dict.keys() 
+                      if known_track_moods_dict[key] in acceptable_moods]
+
+        num_songs_remaining = num_songs_in_arr - song_count
+        if len(ids_to_add) > num_songs_remaining:
+            ids_to_add = ids_to_add[:num_songs_remaining]
+        song_count += len(ids_to_add)
+
         # Add the remaining (newly predicted) song IDs where mood = mood_index
         ids_to_add.extend([key for key in predictions.keys() if predictions[key] in acceptable_moods])
         final_song_ids.extend(ids_to_add)
         # TESTING TO DETERMINE RATE OF SONG ADDITION
         count_mood_index += len([key for key in stat_dict.keys() if stat_dict[key] == mood_index])
         count_other_moods += len(stat_dict) - len(ids_to_add)
-        count_similar_moods += len([key for key in stat_dict.keys() 
-                                    if stat_dict[key] != mood_index 
-                                    and stat_dict[key] in acceptable_moods])
-       
+        count_similar_moods += len([key for key in stat_dict.keys() if stat_dict[key] in 
+                                    [(mood_index + len(moods) - 1) % len(moods), 
+                                     (mood_index + 1)%len(moods)]])
+
         ###TESTING
         print(f"count of specified moods: {count_mood_index}")
         print(f"count of other moods: {count_other_moods}")
         print(f"count of similar moods: {count_similar_moods}")
-        
+
         unique_elements = range(8)
         for elem in unique_elements:
             count = list(stat_dict.values()).count(elem)
@@ -170,7 +176,15 @@ def generateNewSongsList(mood, num_songs, closest_song_ids, old_songs_list): ###
         count_prob = {k: v/sum(counts.values()) for k,v in counts.items()}
         print(f"probability of labels: {count_prob}")
 
+        if len(ids_to_add) == 0: # no new songs were added
+            if number_songs_to_pass > 1:
+                 number_songs_to_pass -= 1
+            elif len(acceptable_moods) < 3:
+                acceptable_moods = [mood_index, (mood_index + len(moods) - 1) % len(moods), (mood_index + 1)%len(moods)]
+            else:
+                num_songs_in_arr = round(num_songs_in_arr/1.5)
 
+                
 #           DB.update_user_liked_songs(UID,predictions.keys()) 
             # ^^Need to add a check to discard songs no longer on spotify, 
             # otherwise we might recommend songs that are no longer on spotify
@@ -182,10 +196,17 @@ def generateNewSongsList(mood, num_songs, closest_song_ids, old_songs_list): ###
 # Returns a list of randomly selected ids which favors the closest songs
 # to the user's centroid
 def generateOldSongsList(num_old_songs, closest_song_ids):
-    if num_old_songs > len(closest_song_ids): return closest_song_ids
+    #check if songs can actually be played in spotify
+    global sp
+    if sp == None:
+        spotify_client()
+        firestoreConnection()
+    tracks = sp.tracks(closest_song_ids)['tracks']
+    track_ids = [track["id"] for track in tracks if track is not None]
+    if num_old_songs > len(track_ids): return track_ids
     song_ids_list = []
     final_list = []
-    songs_to_iterate_over = closest_song_ids.copy()
+    songs_to_iterate_over = track_ids.copy()
     while len(final_list) < num_old_songs:
         song_ids_list = songs_to_iterate_over
         for index, song_id in enumerate(song_ids_list):
@@ -245,14 +266,14 @@ def addTrackMoodToDB(tracks_dict):
         })
     
 #______________________________________________
-# LR Model Classifcation
+# MLP Model Classifcation
 #______________________________________________
 
-def getMoodLabelLR(songFeaturesDF):
-    if LR_model is None:
-        load_LR_model()
-    prediction = LR_model.predict(songFeaturesDF)
-    pred_probability= LR_model.predict_proba(songFeaturesDF)
+def getMoodLabelMLP(songFeatures):
+    if MLP_model is None:
+        load_mlp_model()
+    prediction = MLP_model.predict(songFeatures.values)
+    pred_probability= MLP_model.predict_proba(songFeatures.values)
     return prediction, pred_probability
 
 def retrieveTrackFeatures(track_ids):
@@ -261,15 +282,12 @@ def retrieveTrackFeatures(track_ids):
         # Retrieve track features with current offset
         features = sp.audio_features(track_ids[i:i+50])
         checked_features = [l for l in features if l is not None]
+        # Convert to DataFrame
         if len(checked_features) > 0:
-            # Convert to DataFrame
             df = pd.DataFrame(checked_features)
-            
-    #         # Remove columns that we don't need
-    #         df = df.drop(['type', 'uri', 'analysis_url', 'track_href'], axis=1)
-            
-            df = df[['id', 'valence', 'energy']]
-            
+            # Remove columns that we don't need
+            df = df.drop(['type', 'uri', 'analysis_url', 'track_href'], axis=1)
+
             # Append to list of dataframes
             dfs.append(df)
     
@@ -279,3 +297,44 @@ def retrieveTrackFeatures(track_ids):
     #convert to dictionary, with track id as key
 #     features_dict = features_df.set_index('id').T.to_dict('list')
     return features_df
+
+def clipAndNormalizeMLP(features):
+    #clip the features to the range of the training data
+    features['danceability'] = features['danceability'].clip(lower=0.25336000000000003, upper=0.9188199999999997)
+    features['energy'] = features['energy'].clip(lower=0.047536, upper=0.982)
+    features['loudness'] = features['loudness'].clip(lower=-24.65708, upper=-0.8038200000000288)
+    features['speechiness'] = features['speechiness'].clip(lower=0.0263, upper=0.5018199999999997)
+    features['acousticness'] = features['acousticness'].clip(lower=1.4072e-04, upper=0.986)
+    features['instrumentalness'] = features['instrumentalness'].clip(lower=0.0, upper=0.951)
+    features['liveness'] = features['liveness'].clip(lower=0.044836, upper=0.7224599999999991)
+    features['valence'] = features['valence'].clip(lower=0.038318, upper=0.9348199999999998)
+    features['tempo'] = features['tempo'].clip(lower=66.34576, upper=189.87784)
+    features['duration_ms'] = features['duration_ms'].clip(lower=86120.0, upper=341848.79999999976)
+    features['time_signature'] = features['time_signature'].clip(lower=3.0, upper=5.0)
+    
+    columns_to_log=['liveness', 'instrumentalness', 'acousticness', 'speechiness','loudness','energy']
+
+    for i in columns_to_log:
+        if i == 'loudness':
+            features[i] = features[i] + 60
+        features[i] = np.log(features[i]+1)
+
+    #normalize the data
+    scaler = pickle.load(open('scaler3.pkl', 'rb'))
+    #fit on all columns except the track id
+    preprocessedFeatures = scaler.transform(features)
+
+    #convert to dictionary, with track id as key
+    preprocessedFeatures = pd.DataFrame(preprocessedFeatures, columns=features.columns)
+
+    
+    #apply z-score normalization
+    for i in columns_to_log:
+        preprocessedFeatures[i] = stats.zscore(preprocessedFeatures[i])
+        preprocessedFeatures.clip(lower=-2.7, upper=2.7, inplace=True)
+
+    preprocessedFeatures['id'] = features.index.to_list()
+    preprocessedFeatures.set_index('id', inplace=True)
+
+#     preprocessedFeatures = preprocessedFeatures.set_index('id').T.to_dict('list')
+    return preprocessedFeatures
